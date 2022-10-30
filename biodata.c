55b659e631a37c5f6913aa00f314dc9d494c875d
promamondol <116403134+promamondol@users.noreply.github.com>
2022-10-30 21:07:44 +0600

    Add files via upload

---
+++ biodata.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    char name[20],fathername[20],mothername[20],hometown[20],occupation[50],address[30],highschool[16],college[35],university[50],e_mail[25],hobby[70];
+    printf("enter your name : \n");
+    scanf("%s",&name);
+    printf("enter your fathername : \n");
+    scanf("%s",&fathername);
+    printf("enter your mothername : \n");
+    scanf("%s",&mothername);
+    printf("enter your hometown : \n");
+    scanf("%s",&hometown);
+    printf("enter your occupation : \n");
+    scanf("%s",&occupation);
+    printf("enter your adress : \n");
+    scanf("%s",&address);
+    printf("enter your highschool : \n");
+    scanf("%s",&highschool);
+    printf("enter your college : \n");
+    scanf("%s",&college);
+    printf("enter your university : \n");
+    scanf("%s",&university);
+    printf("enter your e_mail : \n");
+    scanf("%s",&e_mail);
+    printf("enter your hobby : \n");
+    scanf("%s",&hobby);
+}
+
+
+
+
